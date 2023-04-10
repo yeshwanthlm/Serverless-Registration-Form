45ad344f5fdd333d5111a5a8a32beefc8db6e3a7
Yeshwanth | A Monk in Cloud ☁️ <66474973+yeshwanthlm@users.noreply.github.com>
2023-04-10 19:56:37 +0530

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -11,7 +11,7 @@ function submitForm() {
     const xhr = new XMLHttpRequest();
 
     // Set up request
-    xhr.open('POST', 'https://e8rrpb7stg.execute-api.us-east-1.amazonaws.com/prod/register', true);
+    xhr.open('POST', 'API_INVOKE_URL/register', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     // Set up response handler
