e4fbdc71019a5bb0f125d078eddc4e9b0360dfa2
Yeshwanth | A Monk in Cloud ☁️ <66474973+yeshwanthlm@users.noreply.github.com>
2023-04-10 19:54:54 +0530

    Create script.js

---
+++ Frontend/script.js
@@ -0,0 +1,39 @@
+function submitForm() {
+    event.preventDefault();
+
+    // Get form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const password = document.getElementById('password').value;
+
+    // Create request object
+    const xhr = new XMLHttpRequest();
+
+    // Set up request
+    xhr.open('POST', 'https://e8rrpb7stg.execute-api.us-east-1.amazonaws.com/prod/register', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    // Set up response handler
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                alert('Registration successful!');
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('phone').value = '';
+                document.getElementById('password').value = '';
+            } else {
+                alert('Registration failed: ' + xhr.responseText);
+            }
+        }
+    };
+
+    // Send request
+    xhr.send(JSON.stringify({
+        name: name,
+        email: email,
+        phone: phone,
+        password: password
+    }));
+}
